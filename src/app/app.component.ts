63c462b032e06582579344dada4b91888a386d28
micaPrieto <micaelaprieto@outlook.com>
2024-11-01 12:09:24 -0300

    arreglo error de movimiento de carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginRegisterComponent } from './component/login-register/login-register.component';
 import { OlvidasteContraseniaComponent } from './component/olvidaste-contrasenia/olvidaste-contrasenia.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { FooterComponent } from "./component/footer/footer.component";
+import { FooterComponent } from "./component/shared/footer/footer.component";
 import { InicioPageComponent } from './pages/inicio-page/inicio-page.component';
+import { NavbarComponent } from './component/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginRegisterComponent, OlvidasteContraseniaComponent, NavbarComponent, FooterComponent,InicioPageComponent],
+  imports: [RouterOutlet,
+      LoginRegisterComponent,
+     OlvidasteContraseniaComponent,
+      NavbarComponent,
+      FooterComponent,
+      InicioPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
