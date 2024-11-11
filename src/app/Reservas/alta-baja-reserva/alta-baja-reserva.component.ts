29b9801075cd2b86ec3feb0f1d2447e0023e63bd
leoemaanuel <leandrover25@gmail.com>
2024-11-11 12:59:33 -0300

    Comienzo de form para reservar

--- src/app/Reservas/alta-baja-reserva/alta-baja-reserva.component.ts
+++ src/app/Reservas/alta-baja-reserva/alta-baja-reserva.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-alta-baja-reserva',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './alta-baja-reserva.component.html',
   styleUrl: './alta-baja-reserva.component.css'
 })
 export class AltaBajaReservaComponent {
+  reservaForm: FormGroup; 
 
+  constructor(
+    public dialogRef: MatDialogRef<AltaBajaReservaComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, 
+    private fb: FormBuilder 
+  ) {
+
+    this.reservaForm = this.fb.group({
+      fecha: ['', Validators.required], 
+      horario: ['', Validators.required], 
+      direccion: ['', Validators.required] 
+    });
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  realizarReserva(): void {
+    if (this.reservaForm.invalid) return;
+    
+    const reservaData = this.reservaForm.getRawValue; // Obtenemos los datos del formulario
+    this.dialogRef.close(reservaData); // Cerramos el diálogo y devolvemos los datos
+  }
 }
--- src/app/component/Inicio/card/card.component.ts
+++ src/app/component/Inicio/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AltaBajaReservaComponent } from '../../../Reservas/alta-baja-reserva/alta-baja-reserva.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [AltaBajaReservaComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
