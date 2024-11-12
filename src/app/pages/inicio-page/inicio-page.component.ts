c5726fde05381046922e0226746d34bfc12cbd56
micaPrieto <micaelaprieto@outlook.com>
2024-11-12 13:50:47 -0300

    enrutamiento login

--- src/app/pages/inicio-page/inicio-page.component.ts
+++ src/app/pages/inicio-page/inicio-page.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SearchBarComponent } from '../../component/Inicio/search-bar/search-bar.component';
 import { CardComponent } from '../../component/Inicio/card/card.component';
 import { GridComponent } from '../../component/Inicio/grid/grid.component';
 import { NavbarPublicComponent } from "../../shared/navbar-public/navbar-public.component";
 import { NavbarPrivateComponent } from "../../shared/navbar-private/navbar-private.component";
+import { UsuariosService } from '../../service/usuarios.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from '../../interface/usuario';
 
 
 @Component({
@@ -13,6 +16,49 @@ import { NavbarPrivateComponent } from "../../shared/navbar-private/navbar-priva
   templateUrl: './inicio-page.component.html',
   styleUrl: './inicio-page.component.css'
 })
-export class InicioPageComponent {
+export class InicioPageComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.accederAlosDatos();
+  }
+
+  service= inject(UsuariosService);
+  ar= inject(ActivatedRoute);
+  router = inject(Router);
+
+  usuario?: Usuario; // Dejo los datos del usuario acá por si los necesitamos para algo
+  id : string | null = null;
+
+  accederAlosDatos()
+    {
+      this.ar.paramMap.subscribe(
+        {
+          next: (param)=>
+          {
+              this.id = param.get('id'); // Acá se guarda el id sacado de la ruta
+              this.getById()
+          },
+          error: ()=>{
+            alert('Error al acceder a los datos');
+          }
+        }
+      )
+    }
+
+    getById() //ESTO POR AHORA NO SE USA, ya que no hacemos nada con los datos del usuario en la page
+    {
+      this.service.getUsuarioById(this.id).subscribe(
+        {
+          next: (usuario : Usuario)=>
+          {
+            this.usuario = usuario;
+          },
+          error: () =>
+          {
+            alert('Error al acceder a los datos');
+          }
+        }
+    )
+    }
 
 }
