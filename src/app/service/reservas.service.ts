9f16b5d4b452b1d5bd0d50a1a376ce8ec42b190b
abruxx <abrupaez22@gmail.com>
2024-11-13 18:03:01 -0300

    Filtrar por idUs lista de reservas enviadas

--- src/app/service/reservas.service.ts
+++ src/app/service/reservas.service.ts
@@ -2,18 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Reserva } from '../interface/reserva';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservasService {
   urlBase: string = "http://localhost:3000/reservas"
-  constructor(private http:HttpClient) { }
+  constructor(
+    private http:HttpClient,
+    private authService: AuthService
+  ) { }
 
   private reservasSubject = new BehaviorSubject<Reserva[]>([]);
   reservasSer = this.reservasSubject.asObservable();
 
   ////// Funciones Notificaciones /  Alta Baja
+
+  cargarReservas(): void {
+    const userId = this.authService.getUserId();  // Obtenemos el ID del usuario logueado
+
+    if (!userId) {
+      console.log("No hay usuario logueado.");
+      return;  // Si no hay usuario logueado, no hacemos nada
+    }
+
+    // Hacemos la solicitud GET para obtener las reservas
+    this.http.get<Reserva[]>(this.urlBase).subscribe((reservas) => {
+      // Filtramos las reservas según el ID del usuario logueado
+      const reservasFiltradas = reservas.filter(reserva => reserva.idUs === userId);
+      this.reservasSubject.next(reservasFiltradas);  // Actualizamos el estado con las reservas filtradas
+    });
+  }
 
   agregarReserva(reserva: Reserva) {
     const reservas = this.reservasSubject.getValue();
